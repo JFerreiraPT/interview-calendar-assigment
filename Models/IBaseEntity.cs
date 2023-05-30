36ace40891a52f7388db6da240885b950071567c
João Ferreira <joao.p.r.fereira.ofgmail.com>
2023-05-30 11:31:03 +0100

    feat: add user and interviewer models

---
+++ Models/IBaseEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Interview_Calendar.Models
+{
+	public interface IBaseEntity
+	{
+        [BsonId]
+        [BsonRepresentation(BsonType.String)]
+        ObjectId Id { get; set; }
+
+        DateTime CreatedAt { get; }
+        bool isActive { get; set; }
+
+	}
+}
+
---
+++ Models/Interviewer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Interview_Calendar.Models
+{
+	public class Interviewer : User
+	{
+        public Dictionary<DateOnly, SortedSet<int>> Availability = default!;
+		public List<DateTime> Interviews = default!;
+	}
+}
+
