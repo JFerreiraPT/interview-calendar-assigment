242a4464d25e8147687e45be15a4bbad4ab7198e
João Ferreira <joao.p.r.fereira.ofgmail.com>
2023-05-31 15:32:40 +0100

    feat: add class that validates if slot times are between work times

--- DTOs/AvailabilityRequestDTO.cs
+++ DTOs/AvailabilityRequestDTO.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using Interview_Calendar.Helpers;
+
 namespace Interview_Calendar.DTOs
 {
 	public class AvailabilityRequestDTO
 	{
         public DateOnly Date { get; set; }
+        [ValidateTimeSlots]
         public int[]? TimeSlots { get; set; } = default!;
     }
 }
---
+++ Helpers/ValidateTimeSlotsAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Interview_Calendar.Helpers
+{
+    public class ValidateTimeSlotsAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value is int[] timeSlots)
+            {
+                foreach (int timeSlot in timeSlots)
+                {
+                    if (timeSlot < 9 || timeSlot > 18)
+                    {
+                        return new ValidationResult("TimeSlots array contains elements outside the valid range of 9 to 18.");
+                    }
+                }
+            }
+            else if(value != null)
+            {
+                return new ValidationResult("Invalid type. Expected an int[] for TimeSlots array.");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
+
